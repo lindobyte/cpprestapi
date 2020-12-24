8de70c0ed6ae115d1ef1d5170db65230aeabc361
Timo Zurhove <info@tz-develop.de>
2020-12-24 10:45:07 +0100

    implemented class AbstractResource

--- src/resource/customer/Customer.h
+++ src/resource/customer/Customer.h
@@ -1,24 +1,22 @@
 #include "../../webservice.h"
+#include "../AbstractResource.h"
 
 using namespace web;
 using namespace http;
 using namespace utility;
 using namespace http::experimental::listener;
 
-class Customer
+class Customer : public AbstractResource
 {
 public:
-    Customer() {}
+    //Customer() {}
     Customer(utility::string_t url);
-
-    pplx::task<void> open() { return m_listener.open(); }
-    pplx::task<void> close() { return m_listener.close(); }
 
 private:
     void handle_get(http_request message);
-    void handle_put(http_request message);
+    /*void handle_put(http_request message);
     void handle_post(http_request message);
-    void handle_delete(http_request message);
+    void handle_delete(http_request message);*/
 
-    http_listener m_listener;
+    //http_listener m_listener;
 };
